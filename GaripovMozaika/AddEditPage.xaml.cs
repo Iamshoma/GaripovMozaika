5d58a88f286cedb8602c88968047bd4b7e77e898
шамиль <@DESKTOP-IB2VL99>
2025-06-12 14:48:24 +0500

    Добавьте файлы проекта.

---
+++ GaripovMozaika/AddEditPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GaripovMozaika
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+
+        private Material _currentMaterial = new Material();
+        public AddEditPage(Material selectedMaterial)
+        {
+            InitializeComponent();
+
+            if (selectedMaterial != null)
+            {
+                _currentMaterial = selectedMaterial;
+                ComboType.SelectedIndex = _currentMaterial.Material_Type - 1;
+            }
+            DataContext = _currentMaterial;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+          manager.MainFrame.GoBack();
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComboType.SelectedItem != null)
+            {
+                _currentMaterial.Material_Type = ComboType.SelectedIndex + 1;
+            }
+
+            if (_currentMaterial.Material_ID == 0)
+            {
+                GaripovMozaikaEntities.GetContext().Material.Add(_currentMaterial);
+            }
+
+            try
+            {
+
+                GaripovMozaikaEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информцаия сохранена");
+                manager.MainFrame.GoBack();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ GaripovMozaika/MaterialPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GaripovMozaika
+{
+    /// <summary>
+    /// Логика взаимодействия для MaterialPage.xaml
+    /// </summary>
+    public partial class MaterialPage : Page
+    {
+        public MaterialPage()
+        {
+            InitializeComponent();
+
+            var currentMaterial = GaripovMozaikaEntities.GetContext().Material.ToList();
+            ListViewMaterial.ItemsSource = currentMaterial;
+        }
+
+        private void AddMaterial_Click(object sender, RoutedEventArgs e)
+        {
+
+            manager.MainFrame.Navigate(new AddEditPage(null));  
+
+        }
+
+        private void EditMaterial_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Material));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                GaripovMozaikaEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(entry => entry.Reload());
+                ListViewMaterial.ItemsSource = GaripovMozaikaEntities.GetContext().Material.ToList();
+            }
+        }
+    }
+}
